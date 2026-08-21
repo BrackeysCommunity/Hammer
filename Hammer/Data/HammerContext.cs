afa5cd8c771d985a0f4fa9bc791c507111cb90d6
Oliver Booth <oliver@booth.dev>
2026-08-21 21:54:34 +0100

    refactor: compute DbSet properties

--- Hammer/Data/HammerContext.cs
+++ Hammer/Data/HammerContext.cs
@@ -20,73 +20,106 @@ internal sealed class HammerContext : DbContext
     ///     Gets the set of alt accounts.
     /// </summary>
     /// <value>The set of alt accounts.</value>
-    public DbSet<AltAccount> AltAccounts { get; private set; } = null!;
+    public DbSet<AltAccount> AltAccounts
+    {
+        get => Set<AltAccount>();
+    }
 
     /// <summary>
     ///     Gets the set of users who are blocked from making reports.
     /// </summary>
     /// <value>The set of blocked reporters.</value>
-    public DbSet<BlockedReporter> BlockedReporters { get; private set; } = null!;
+    public DbSet<BlockedReporter> BlockedReporters
+    {
+        get => Set<BlockedReporter>();
+    }
 
     /// <summary>
     ///     Gets the set of staff-deleted messages.
     /// </summary>
     /// <value>The set of staff-deleted messages.</value>
-    public DbSet<DeletedMessage> DeletedMessages { get; private set; } = null!;
+    public DbSet<DeletedMessage> DeletedMessages
+    {
+        get => Set<DeletedMessage>();
+    }
 
     /// <summary>
     ///     Gets the set of infractions.
     /// </summary>
     /// <value>The set of infractions.</value>
-    public DbSet<Infraction> Infractions { get; private set; } = null!;
+    public DbSet<Infraction> Infractions
+    {
+        get => Set<Infraction>();
+    }
 
     /// <summary>
     ///     Gets the set of member notes.
     /// </summary>
     /// <value>The set of member notes.</value>
-    public DbSet<MemberNote> MemberNotes { get; private set; } = null!;
+    public DbSet<MemberNote> MemberNotes
+    {
+        get => Set<MemberNote>();
+    }
 
     /// <summary>
     ///     Gets the set of mutes.
     /// </summary>
     /// <value>The set of mutes.</value>
-    public DbSet<Mute> Mutes { get; private set; } = null!;
+    public DbSet<Mute> Mutes
+    {
+        get => Set<Mute>();
+    }
 
     /// <summary>
     ///     Gets the set of reported messages.
     /// </summary>
     /// <value>The set of reported messages.</value>
-    public DbSet<ReportedMessage> ReportedMessages { get; private set; } = null!;
+    public DbSet<ReportedMessage> ReportedMessages
+    {
+        get => Set<ReportedMessage>();
+    }
 
     /// <summary>
     ///     Gets the set of rules.
     /// </summary>
     /// <value>The set of rules.</value>
-    public DbSet<Rule> Rules { get; private set; } = null!;
+    public DbSet<Rule> Rules
+    {
+        get => Set<Rule>();
+    }
 
     /// <summary>
     ///     Gets the set of staff messages.
     /// </summary>
     /// <value>The set of staff messages.</value>
-    public DbSet<StaffMessage> StaffMessages { get; private set; } = null!;
+    public DbSet<StaffMessage> StaffMessages
+    {
+        get => Set<StaffMessage>();
+    }
 
     /// <summary>
     ///     Gets the set of temporary bans.
     /// </summary>
     /// <value>The set of temporary bans.</value>
-    public DbSet<TemporaryBan> TemporaryBans { get; private set; } = null!;
+    public DbSet<TemporaryBan> TemporaryBans
+    {
+        get => Set<TemporaryBan>();
+    }
 
     /// <summary>
     ///     Gets the set of tracked messages.
     /// </summary>
     /// <value>The set of tracked messages.</value>
-    public DbSet<TrackedMessage> TrackedMessages { get; private set; } = null!;
+    public DbSet<TrackedMessage> TrackedMessages
+    {
+        get => Set<TrackedMessage>();
+    }
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
+
         modelBuilder.HasDefaultSchema("hammer");
 
         modelBuilder.ApplyConfiguration(new AltAccountConfiguration());
